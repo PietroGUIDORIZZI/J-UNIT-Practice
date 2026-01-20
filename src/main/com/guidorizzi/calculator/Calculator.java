2b11bfe21765607751991df4922dd3fec8506a8b
Pietro Guidorizzi <peterguidorizzi@gmail.com>
2026-01-20 10:18:12 -0300

    Testing setup.

---
+++ src/main/com/guidorizzi/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.guidorizzi.calculator;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Calculator {
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
---
+++ src/test/com/guidorizzi/calculator/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.guidorizzi.calculator;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void canAddZeroPlusZero(){
+        Calculator calc = new Calculator();
+        int sum = calc.add(0, 0);
+        assertEquals(0, sum, "Was expecting sum of 0");
+    }
+
+
+
+
+
+}
+
+
+
